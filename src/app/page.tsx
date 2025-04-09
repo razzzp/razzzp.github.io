c6f46096160748b6cf666aa2f64768a115a6b796
Razif <razif.pramuda@shopee.com>
2025-04-09 16:52:56 +0700

    add sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,33 @@ export default function Home() {
 
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-base-200 text-base-content p-4">
-      <div className="text-center space-y-4">
-        <h1 className="text-4xl font-bold">Hello 👋</h1>
-        <p className="text-xl">{"I'm building my personal website with Next.js + Tailwind + DaisyUI"}</p>
+      <section className="text-center space-y-4">
+        <h2 className="text-4xl">Hi 👋, I'm <span className="font-bold">Razif</span> </h2>
+        <p className="text-xl">I am a Software Engineer who is always curious and excited to learn new things</p>
         <button className="btn btn-primary">Get in touch</button>
-      </div>
+      </section>
+
+      <section className="py-10">
+        <h2 className="text-4xl pd-4 pt-8">Projects</h2>
+        <div className="py-4">
+          <h3 className="text-2xl pb-2 pt-4">Project 1</h3>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente at soluta maxime perspiciatis nam, et, harum porro atque vero quas esse distinctio pariatur modi nihil illo reiciendis voluptatem rerum necessitatibus?</p>
+        </div>
+      </section>
+
+      <section className="py-10">
+        <h2 className="text-4xl pd-4 pt-8">Education</h2>
+        <div className="py-4">
+          <h3 className="text-2xl pb-2 pt-4">Hacktiv8</h3>
+          <h5>Go Backend</h5>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente at soluta maxime perspiciatis nam, et, harum porro atque vero quas esse distinctio pariatur modi nihil illo reiciendis voluptatem rerum necessitatibus?</p>
+        </div>
+        <div className="py-4">
+          <h3 className="text-2xl pb-2 pt-4">University of Nottingham</h3>
+          <h5>MEng Electrical and Electronic Engineering</h5>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente at soluta maxime perspiciatis nam, et, harum porro atque vero quas esse distinctio pariatur modi nihil illo reiciendis voluptatem rerum necessitatibus?</p>
+        </div>
+      </section>
 
       {/* Floating Collapsible Menu */}
       <div className="fixed bottom-6 right-6 z-50">
