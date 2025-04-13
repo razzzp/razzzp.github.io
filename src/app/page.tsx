451c5e80a9bbd92392379cb6ec4ef6d3cca44342
razzzp <m.razif.pramuda@gmail.com>
2025-04-13 22:24:57 +0700

    projects & edu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, ReactNode, useState } from "react";
 import Link from "next/link";
 import { FaBars, FaTimes } from "react-icons/fa";
 
@@ -16,15 +16,14 @@ function NavItemBracket(props: {text: string,link: string, key:number}){
   const [hover, setHover] = useState(false)
 
   const text = hover ? `[ ${props.text} ]` : props.text
-  return (<li className="px-2 min-w-40 text-center"><a onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)} className="underline
-    decoration-transparent underline-offset-[10px] decoration-2
-    transition-all ease-in" href={props.link}>{text}</a></li>)
+  return (<li className="px-2 min-w-40 text-center"><a onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)} 
+    className="" href={props.link}>{text}</a></li>)
 }
 
 function NavBar(props: {items : {text: string,link: string, key:number}[]}){
   return (
     <>
-      <nav className="top-0 inset-x-0 fixed py-4 px-4 bg-base-300 min-w-screen">
+      <nav className="sticky top-0 py-4 px-20 bg-base-300 w-screen">
         <ul className="flex justify-center">
           {
             props.items.map((item) => {
@@ -37,12 +36,28 @@ function NavBar(props: {items : {text: string,link: string, key:number}[]}){
   )
 }
 
-function FullPaddedSection(props :PropsWithChildren) {
+function FullPaddedSection(props :{children:ReactNode, secId?: string}) {
   return (
     <>
-    <section className="h-screen px-30">
+    <section className="min-h-screen min-w-screen px-30">
+        {/* dummy sec for anchor */}
+        <div id={props.secId} className="h-4"></div>
         {props.children}
     </section>
+    </>
+  )
+}
+
+function TagsList(props: {tags: string[]}){
+  return(
+    <>
+    <ul>
+      {
+        props.tags.map((tag, idx, arr)=>{
+          return <li key={idx} className="badge badge-secondary mr-2 mb-1">{tag}</li>
+        })
+      }
+    </ul>
     </>
   )
 }
@@ -55,38 +70,92 @@ export default function Home() {
       <NavBar items={[
         {text: "Projects", link: "#projects", key:1},
         {text: "Experience", link: "#experience", key:2},
-        {text: "About Me", link: "#aboutme", key:3},
-        {text: "Contact", link: "#contacts", key:4},
+        {text: "Education", link: "#education", key:3},
+        {text: "Contact", link: "#contact", key:4},
         ]}/>
       <main className="min-h-screen flex flex-col items-center justify-center bg-base-300 text-base-content">
         <FullPaddedSection>
-          <div className="space-y-4 h-full flex flex-col justify-center">
+          <div className="space-y-4 h-screen flex flex-col justify-center -top-30 relative">
             <h2 className="text-4xl">Hi 👋,<br/> I'm <span className="font-bold text-primary">Razif</span> </h2>
             <p className="text-xl text-base-content/70">A Software Engineer who is always curious and excited to learn new things</p>
-            <button className="btn btn-primary col-2">Get in touch</button>
+            <button className="btn btn-primary w-50">Get in touch</button>
           </div>
         </FullPaddedSection>
 
-        <FullPaddedSection>
+        <FullPaddedSection secId="projects">
           <h2 className="text-4xl pd-4 pt-8">Projects</h2>
-          <div className="py-4">
-            <h3 className="text-2xl pb-2 pt-4">Project 1</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente at soluta maxime perspiciatis nam, et, harum porro atque vero quas esse distinctio pariatur modi nihil illo reiciendis voluptatem rerum necessitatibus?</p>
+          <div className="py-4 space-y-4">
+            <h3 className="text-2xl pt-4 mb-1 text-primary">Breathe.io</h3>
+            <p className="text-neutral-content">Breathe.io is a platform offering air quality monitoring and insights, designed for both individuals and businesses. 
+              It includes features like real-time alerts, detailed air quality reports, and environmental recommendations.</p>
+            <a className="link-info block" href="https://github.com/h8-breathe-io/breathe-io">Github</a>
+            <TagsList tags={
+              [
+                "Go","Echo","PostgreSQL","gRPC","Xendit","Mailtrap", "Open-Meteo API"
+              ]
+            }/>
+          </div>
+
+          <div className="py-4 space-y-4">
+            <h3 className="text-2xl pt-4 mb-1 text-primary">Car Rental Management App Backend</h3>
+            <p className="text-neutral-content">Backend for a Car Rental Management Application.<br/>
+              Provide features to manage the business’ car fleet, allow users to make rental bookings, 
+              make payments and notify important events to users by email.</p>
+            <a className="link-info block" href="https://github.com/razzzp/h8-p2-finalproj">Github</a>
+            <TagsList tags={
+              [
+                "Go","Echo","PostgreSQL","GORM","Xendit","Google SMTP","Testify"
+              ]
+            }/>
+          </div>
+
+          <div className="py-4 space-y-4">
+            <h3 className="text-2xl pt-4 mb-1 text-primary">LSP (Language Server Protocol) Server for a Domain Specific Language</h3>
+            <p className="text-neutral-content">Lexer, Parser and LSP Server for a Domain Specific Language using Rust.<br/>
+              Implemented Linter/Analyzer to check for programming rules, provide Goto Definition, Auto-complete and other features<br/>
+              Utilized multi-threading to allow analysis of projects containing 50,000+ source files.</p>
+            <a className="link-info block" href="https://github.com/razzzp/gold-lang-lsp">Github</a>
+            <TagsList tags={
+              [
+                "Rust","LSP","Multi-threading"
+              ]
+            }/>
+          </div>
+
+          <div className="py-4 space-y-4">
+            <h3 className="text-2xl pt-4 mb-1 text-primary">Dog Kennel Monitoring System</h3>
+            <p className="text-neutral-content">Modular dog kennel monitoring system that can measure the temperature and humidity of dog kennels in real time. <br/>
+              Designed the system for easy implementation in a dog shelter environment and scalable up to hundreds of kennels.</p>
+            {/* <a className="link-info block" href="https://github.com/razzzp/gold-lang-lsp">Github</a> */}
+            <TagsList tags={
+              [
+                "C++","Qt"
+              ]
+            }/>
           </div>
         </FullPaddedSection>
 
-        <FullPaddedSection>
-          <h2 className="text-4xl pd-4 pt-8">Education</h2>
+        <FullPaddedSection secId="Experience">
+          <h2 className="text-4xl pd-4 pt-8">Experience</h2>
+          TODO
+        </FullPaddedSection>
+
+        <FullPaddedSection secId="education">
+          <h2 className="text-4xl pd-4 pt-8" id="education">Education</h2>
           <div className="py-4">
-            <h3 className="text-2xl pb-2 pt-4">Hacktiv8</h3>
-            <h5>Go Backend</h5>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente at soluta maxime perspiciatis nam, et, harum porro atque vero quas esse distinctio pariatur modi nihil illo reiciendis voluptatem rerum necessitatibus?</p>
+            <h3 className="text-2xl text-primary">Hacktiv8</h3>
+            <h4 className="text-secondary-content">Go Backend</h4>
           </div>
+
           <div className="py-4">
-            <h3 className="text-2xl pb-2 pt-4">University of Nottingham</h3>
-            <h5>MEng Electrical and Electronic Engineering</h5>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente at soluta maxime perspiciatis nam, et, harum porro atque vero quas esse distinctio pariatur modi nihil illo reiciendis voluptatem rerum necessitatibus?</p>
+            <h3 className="text-2xl text-primary">University of Nottingham</h3>
+            <h4 className="text-secondary-content">MEng Electrical and Electronic Engineering</h4>
           </div>
+        </FullPaddedSection>
+
+        <FullPaddedSection secId="contact">
+          <h2 className="text-4xl pd-4 pt-8" id="contact">Contact</h2>
+          TODO
         </FullPaddedSection>
 
         {/* Floating Collapsible Menu */}
@@ -124,22 +193,6 @@ export default function Home() {
             )}
           </div>
         </div>
-
-        {/* Dummy Sections
-        <section id="about" className="min-h-screen p-8 bg-base-100 mt-10 rounded-box">
-          <h2 className="text-2xl font-bold mb-2">About Me</h2>
-          <p>Write something interesting about yourself here.</p>
-        </section>
-
-        <section id="portfolio" className="min-h-screen p-8 bg-base-100 mt-10 rounded-box">
-          <h2 className="text-2xl font-bold mb-2">Portfolio</h2>
-          <p>Show off your work or side projects here.</p>
-        </section>
-
-        <section id="blogs" className="min-h-screen p-8 bg-base-100 mt-10 rounded-box">
-          <h2 className="text-2xl font-bold mb-2">Blogs</h2>
-          <p>Share your thoughts, tutorials, or articles.</p>
-        </section> */}
     </main>
     </>
   );
