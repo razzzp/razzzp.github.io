cf8b4e4cbda187057596997327f4ed51b930849b
razzzp <m.razif.pramuda@gmail.com>
2025-04-20 19:04:32 +0700

    expandable in timeline

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -88,9 +88,7 @@ export const experiences : Experience[] = [
         title: "Software Engineer",
         content: (
           <>
-          <ul>
-            <li></li>
-          </ul>
+            <li>Backend Homepage</li>
           </>
         )
       }
@@ -99,19 +97,17 @@ export const experiences : Experience[] = [
   {
     title: "IDEMIA",
     location: "Jakarta, Indonesia",
-    startDate: new Date(2022,2),
-    endDate: new Date(2025,2),
+    startDate: new Date(2022,1),
+    endDate: new Date(2025,1),
     positions: [
       {
         title: "Software Development Engineer III",
         content: (
           <>
-            <ul>
               <li>Designed and reviewed software architecture documents.</li>
               <li>Led project initiation, analyzing requirements, architecture, and new technologies.</li>
               <li>Ensured development team readiness through knowledge sharing and training.</li>
               <li>Oversaw project execution in collaboration with testers, product owners, and stakeholders.</li>
-            </ul>
           </>
         )
       },
@@ -119,7 +115,6 @@ export const experiences : Experience[] = [
         title: "Software Development Engineer II",
         content: (
           <>
-            <ul>
               <li>Lead development teams with up to 7 members (developers & testers) to deliver 38 features.</li>
               <li>Achieved &gt; 90% on time delivery.</li>
               <li>Took up product owner roles for several features.</li>
@@ -129,7 +124,6 @@ export const experiences : Experience[] = [
               <li>Developed code analyzer to reduce code review time, by automatically checking programming rules.</li>
               <li>Held knowledge sharing sessions and mentoring to increase team knowledge.</li>
               <li>Contributed to the decrease of 40% issues found in production.</li>
-            </ul>
           </>
         )
       },
@@ -137,11 +131,9 @@ export const experiences : Experience[] = [
         title: "Software Development Engineer I",
         content: (
           <>
-            <ul>
               <li>12 Features developed with &gt;90% on time delivery, and &gt;85% first time right.</li>
               <li>38 Bugs resolved with &gt;90% meeting SLA.</li>
               <li>10 improvement ideas proposed &amp; implemented.</li>
-            </ul>
           </>
         )
       }
@@ -157,11 +149,9 @@ export const experiences : Experience[] = [
         title: "Intern",
         content: (
           <>
-            <ul>
-              <li>Tested biometric products and ensured that they function according to the company’s standards.</li>
-              <li>Troubleshooted and repaired faulty devices.</li>
-              <li>Compiled reports showing all the updated features available in the company’s product listing, to ensure clarity in the documentation.</li>
-            </ul>
+            <li>Tested biometric products and ensured that they function according to the company’s standards.</li>
+            <li>Troubleshooted and repaired faulty devices.</li>
+            <li>Compiled reports showing all the updated features available in the company’s product listing, to ensure clarity in the documentation.</li>
           </>
         )
       }
